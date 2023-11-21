eb9c72c8b71b5d1479a9d49742c89a3c78445a4f
Fauzi Mulyana <94151536+fauzimulyanaa@users.noreply.github.com>
2023-11-21 20:21:50 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const users = require("./router/users");
 const category = require("./router/category");
 const auth = require("./router/auth");
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   optionsSuccessStatus: 200,
 };
 
