5cf440b26631e8e63280b936d0f8e2896cb43c89
Fauzi Mulyana <94151536+fauzimulyanaa@users.noreply.github.com>
2023-11-22 12:22:48 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ const recipes = require("./router/recipes");
 const users = require("./router/users");
 const category = require("./router/category");
 const auth = require("./router/auth");
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
-app.use(cors(corsOptions));
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.status(204).end();
+});
 
 
 
